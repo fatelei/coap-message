d04c70c6d55f4e74f839474bd172eaca47a8a37f
wanglei <fatelei@gmail.com>
2014-09-07 16:31:20 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // main entry
 
-var Message = require("./lib/message");
+var Message = require('./lib/message');
 var generateMsgId = require('./lib/msgid').generateMsgId;
+var IncomeMessage = reuqire('./lib/in');
+var OutcomeMessage = require('./lib/out');
 
 exports.generateMsgId = generateMsgId;
 exports.Message = Message;
+exports.IncomeMessage = IncomeMessage;
+exports.OutcomeMessage = OutcomeMessage;
